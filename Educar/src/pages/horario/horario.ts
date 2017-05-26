e95f29173143309616e2f18b0eda62b37b31fcf8
IagoGomes <hiago_pacheco12@hotmail.com>
2017-05-26 16:32:57 -0300

    adicionando as páginas

--- Educar/src/pages/horario/horario.ts
+++ Educar/src/pages/horario/horario.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the Horario page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
 @Component({
   selector: 'page-horario',
-  templateUrl: 'horario.html'
+  templateUrl: 'horario.html',
 })
-export class HorarioPage {
+export class Horario {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Horario');
   }
 
 }
