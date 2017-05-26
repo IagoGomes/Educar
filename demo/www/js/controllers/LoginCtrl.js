04fe3f30e7f171a82c05c00f1d853b113c8d756c
Yuri Silva <yurirssilva@gmail.com>
2017-05-26 15:21:21 -0300

    update myreader

--- demo/www/js/controllers/LoginCtrl.js
+++ demo/www/js/controllers/LoginCtrl.js
@@ -1,4 +1,10 @@
-﻿app.controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+﻿// import { Component } from '@angular/core';
+// import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+// import { Http } from '@angular/http';
+// import 'rxjs/add/operator/map';
+// import { ContentPage } from '../content/content';
+
+app.controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
 	 $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
     $scope.isExpanded = false;
