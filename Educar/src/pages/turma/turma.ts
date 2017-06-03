bd071749165950f22f7b318b3adb45c47ca4895f
IagoGomes <hiago_pacheco12@hotmail.com>
2017-06-03 00:10:47 -0300

    adicionando elementos na janela turma

--- Educar/src/pages/turma/turma.ts
+++ Educar/src/pages/turma/turma.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the Turma page.
@@ -13,12 +15,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'turma.html',
 })
 export class Turma {
+  local : string;
+  sala : string;
+  disciplina :string;
+  quant_alunos : any;
+  horario : string;
+  alunos : Array <any>;
+  idTurma : any;
+  habilitarLancarNota : boolean;
+  notaA: any;
+  hidden:boolean;
+  exibirForm:boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+	this.local = "OI";
+        this.sala = "OI";
+        this.disciplina = "OI";
+        this.quant_alunos = 3;
+        this.horario = "3:00";
+        this.alunos = ['a', 'n'];
+        this.notaA=0;
+        this.hidden=true;
+        
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Turma');
   }
 
+  itemSelected(item: any) {
+    if(this.habilitarLancarNota){
+        this.hidden=!this.hidden;
+    }     
+  }
+
+  fabLancarNota(){
+      this.habilitarLancarNota = !this.habilitarLancarNota;
+  }
+  mudarCorFab(){	
+   	return (this.habilitarLancarNota) ? 'fabAzul' : 'fabVermelha';
+  }
+
+ mudarCorMedia(media : any){
+ 	return (media >= 7) ? "badgeAprovado" : ((media>=0) ? 'badgeReprovado' : 'badgeSemResultado');
+ }
 }
